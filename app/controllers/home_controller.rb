513a4257bdbf2c8b2223bbba0d4449f3f5bb5673
Ubuntu <dougiesal@gmail.com>
2017-11-03 02:26:43 +0000

    Set up manual form for inputting x,y,units

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,25 +5,30 @@ class HomeController < ApplicationController
 def index
 end
 
-def upload
+def click
+  @video = params[:video].tempfile.path
+end
+
+def analyze
   # Get all necessary params
   script = Rails.root.join('lib', 'assets', 'tracker_web.py')
-  path = params[:file].tempfile.path
+  path = params[:video]
   sampling_radius = 10
   tolerance = 75
-  units = 1
-  x = 552
-  y = 459
+  @units = params[:units]
+  length = params[:length].to_i
+  x = params[:x].to_i
+  y = params[:y].to_i
   ext = File.extname(path)
 
   # Names for files to be created
   outname = File.basename(path, ext) + "-out" + ext
   outpath = Rails.root.join('public', 'videos', outname)
-  @video = webm = File.basename(path, ext) + "-out" + ".webm"
+  @output = webm = File.basename(path, ext) + "-out" + ".webm"
   webm_path = Rails.root.join('public', 'videos', webm)
   
   # Run the script
-  @po, @pe, @ps = Open3.capture3("python #{script} #{path} #{sampling_radius} #{tolerance} #{units} #{x} #{y} #{outpath}")
+  @po, @pe, @ps = Open3.capture3("python #{script} #{path} #{sampling_radius} #{tolerance} #{length} #{x} #{y} #{outpath}")
 
   # Convert the file to webm
   @vo, @ve, @vs = Open3.capture3("ffmpeg -i #{outpath} #{webm_path}")
