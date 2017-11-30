2e74f7716cf0028b3901ee568bd2c9fa8891f9f1
doug-salvati <dougiesal@gmail.com>
2017-11-30 20:43:34 +0000

    Disable API

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post '/click' => 'home#click'
   post '/analyze' => 'home#analyze'
 
-  post '/api/upload' => 'api#upload'
+  # post '/api/upload' => 'api#upload'
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
